e183fc8c8d14486b2b8166678a1401e9359b146b
Farokhlagha <fl.razeghi@gmail.com>
2023-07-12 18:47:10 +0100

    Ex

--- PyAssignment2/Ex2_7_fibonachi.py
+++ PyAssignment2/Ex2_7_fibonachi.py
@@ -26,24 +26,24 @@ elif number <=0:
 
 
 
-# n=int(input("please enter number of Fibonacci series: "))
+# # n=int(input("please enter number of Fibonacci series: "))
 
-# f= [1]
+# # f= [1]
 
-# if n==1:
-#     print(f)
+# # if n==1:
+# #     print(f)
     
-# elif n==2:
-#     f.append(1)
-#     print(f)
+# # elif n==2:
+# #     f.append(1)
+# #     print(f)
     
-# elif n>2:
-#     f.append(1)
-#     for i in range(2,n):
-#         fi=f[i-1]+f[i-2]
-#         f.append(fi)
+# # elif n>2:
+# #     f.append(1)
+# #     for i in range(2,n):
+# #         fi=f[i-1]+f[i-2]
+# #         f.append(fi)
     
-#     print(f)
+# #     print(f)
 
-# elif n<=0:
-#     print("Invalid input")
+# # elif n<=0:
+# #     print("Invalid input")
