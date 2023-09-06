4e0dfaeb0b6494002a0b35459b7262ae7cfe102d
Farokhlagha <137986670+Farokhlagha@users.noreply.github.com>
2023-09-06 21:48:03 +0100

    Delete PyAssign4 directory

--- PyAssign4/ex4_1_QRcode.py
+++
@@ -1,9 +0,0 @@
-# Make QRcode with Name and Phone number
-
-import qrcode
-
-name = input("please enter your name: ")
-mobile = input("please enter your mobile number: ")
-
-qr_img= qrcode.make(name + mobile)
-qr_img.save("qr_code.jpg")
